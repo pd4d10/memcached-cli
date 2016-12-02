ab0e8109588a33015cc1ec5847337e084d1cd947
pd4d10 <pd4d10@gmail.com>
2016-12-03 00:12:12 +0800

    Use vorpal instead

--- index.js
+++ index.js
@@ -1,37 +1,67 @@
+const co = require('co')
 const memjs = require('memjs')
-const inquirer = require('inquirer')
+const vorpal = require('vorpal')()
 
 const client = memjs.Client.create()
 
-function parse(input) {
-  const arr = input.split(/\s+/)
-  return {
-    method: arr[0],
-    key: arr[1],
-  }
-}
-
-function query(option, cb) {
-  switch (option.method) {
-    case 'get':
-      client.get(option.key, cb)
-      break;
-    default:
-      break;
-  }
-
-  client.close()
-}
-
-inquirer.prompt([
-  {
-    type: 'input',
-    name: 'command',
-    message: 'memcached >',
+const config = {
+  get: {
+    command: 'get <key>',
+    description: 'GET',
   },
-]).then(input => {
-  const option = parse(input.command)
-  query(option, (err, data) => {
-    console.log(data)
-  })
+  set: {
+    command: 'set <key> <value> [expires]',
+    description: 'SET',
+  },
+  add: {
+    command: 'add <key> <value> [expires]',
+    description: 'ADD',
+  },
+  replace: {
+    command: 'replace <key> <value> [expires]',
+    description: 'replace',
+  },
+}
+
+function parse(data) {
+  try {
+    return data.toString()
+  } catch (err) {
+    return data
+  }
+}
+
+const methods = Object.keys(config)
+
+methods.forEach((method) => {
+  const option = config[method]
+
+  vorpal
+    .command(option.command)
+    .description(option.description)
+    .action(function (args) {
+      let promise
+
+      switch (method) {
+        case 'get':
+          promise = co(function* () {
+            return yield cb => client.get(args.key, cb)
+          })
+          break
+        case 'set':
+        case 'add':
+        case 'replace':
+          promise = co(function* () {
+            return yield cb => client[method](args.key, args.value, cb, args.expires)
+          })
+          break
+      }
+
+      return promise.then((data) => {
+        this.log(parse(data.toString()))
+        client.close()
+      })
+    })
 })
+
+vorpal.delimiter('memcached >').show()
